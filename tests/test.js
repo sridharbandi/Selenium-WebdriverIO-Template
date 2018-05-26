c3d563049e42ff267eb539502fb64aa565f7a29d
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-05-26 22:05:05 +0100

    Initial Commit

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+var webdriverio = require('webdriverio');
+var options = { desiredCapabilities: { browserName: 'chrome' } };
+var client = webdriverio.remote(options);
+
+client
+    .init()
+    .url('https://duckduckgo.com/')
+    .setValue('#search_form_input_homepage', 'WebdriverIO')
+    .click('#search_button_homepage')
+    .getTitle().then(function(title) {
+    console.log('Title is: ' + title);
+
+    // outputs:
+    // "Title is: WebdriverIO (Software) at DuckDuckGo"
+})
+    .end();
